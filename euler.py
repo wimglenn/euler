8bb7800c0443d54a6f4ecb9cf136646b83d71443
wim glenn <hey@wimglenn.com>
2013-12-29 16:26:39 +0000

    p51 and p52

--- euler.py
+++ euler.py
@@ -2,11 +2,12 @@
 
 # http://projecteuler.net/
 
-import math
-import timeit
-import time
 import os
 import ast
+import math
+import time
+import string
+import timeit
 import collections
 import itertools as it
 import operator as op
@@ -1249,6 +1250,43 @@ def p050():
       else:
         break
     n -= 1
+
+
+def p051():
+  """By replacing the 1st digit of the 2-digit number *3, it turns out that six of the nine possible values: 13, 23, 43, 53, 73, and 83, are all prime.
+
+  By replacing the 3rd and 4th digits of 56**3 with the same digit, this 5-digit number is the first example having seven primes among the ten generated numbers, yielding the family: 56003, 56113, 56333, 56443, 56663, 56773, and 56993. Consequently 56003, being the first member of this family, is the smallest prime with this property.
+
+  Find the smallest prime which, by replacing part of the number (not necessarily adjacent digits) with the same digit, is part of an eight prime value family."""
+  # it will never make sense to replace the last digit, because of even numbers
+
+  def replacements(n, positions):
+    template = list(str(n))
+    for position in positions:
+      template[position] = '?'
+    for d in string.digits[1 if 0 in positions else 0:]:
+      yield int(''.join(template).replace('?', d))
+
+  some_primes = primes(10**6)  # primes below 10 million
+  set_primes = set(some_primes)
+  for p in some_primes:
+    n = len(str(p))
+    for i in range(1, n):
+      for indices in it.combinations(range(n - 1), i):
+        hits = [x for x in replacements(p, indices) if x in set_primes]
+        if len(hits) == 8:
+          return sorted(hits)[0]
+
+
+def p052():
+  """It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.
+
+  Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits."""
+  for n in it.count(1):
+    strs = [str(i * n) for i in range(1, 7)]
+    if len(set([''.join(sorted(s)) for s in strs])) == 1:
+      return n
+
 
 
 if __name__ == '__main__' and 1:
