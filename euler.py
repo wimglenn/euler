3b240e105f979e8a24f66db45058fa4558dfecea
wim glenn <hey@wimglenn.com>
2013-12-29 05:14:36 +0000

    49

--- euler.py
+++ euler.py
@@ -1209,6 +1209,25 @@ def p048():
   return int(str(sum(i**i for i in range(1, 1001)))[-10:])
 
 
+def p049():
+  """The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways: (i) each of the three terms are prime, and, (ii) each of the 4-digit numbers are permutations of one another.
+
+  There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property, but there is one other 4-digit increasing sequence.
+
+  What 12-digit number do you form by concatenating the three terms in this sequence?"""
+  primes4 = set(primes(10000)[len(primes(1000)):])  # 4-digit primes
+  for p in primes4:
+    p1 = 3330 + p
+    p2 = 3330 + p1
+    if p1 in primes4 and p2 in primes4:
+      if sorted(str(p1)) == sorted(str(p2)) == sorted(str(p)):
+        if p != 1487:
+          return p2 + p1 * 10**4 + p * 10**8
+
+
+def p050():
+  pass
+
 
 
 if __name__ == '__main__' and 1:
