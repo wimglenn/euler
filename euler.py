bd9a0b66be3e4ad81238bfab4d706dc47c3573c2
wim glenn <hey@wimglenn.com>
2015-05-04 22:42:24 +1000

    053

--- euler.py
+++ euler.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# coding: utf-8
 
 """
 http://projecteuler.net/
@@ -275,6 +276,12 @@ def unique_digits(n):
     >>> unique_digits(1233)
     False"""
     return len(str(n)) == len(set(str(n)))
+
+
+def nCr(n, r, memo={}):
+    if (n, r) not in memo:
+        memo[n, r] = memo[n, n-r] = math.factorial(n) / (math.factorial(r) * math.factorial(n-r))
+    return memo[n, r]
 
 
 #------------------------------------------------------------------------------
@@ -1308,6 +1315,25 @@ def p052():
         strs = [str(i * n) for i in range(1, 7)]
         if len(set([''.join(sorted(s)) for s in strs])) == 1:
             return n
+
+
+def p053():
+    """"There are exactly ten ways of selecting three from five, 12345:
+
+    123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
+
+    In combinatorics, we use the notation, 5C3 = 10.
+
+    In general,
+
+    nCr = n! / r! (n−r)!
+
+    where r ≤ n, n! = n×(n−1)×...×3×2×1, and 0! = 1.
+
+    It is not until n = 23, that a value exceeds one-million: 23C10 = 1144066.
+
+    How many, not necessarily distinct, values of  nCr, for 1 ≤ n ≤ 100, are greater than one-million?"""
+    return sum(1 for n in xrange(1, 101) for r in xrange(1, n) if nCr(n, r) > 1000000)
 
 
 if __name__ == '__main__' and 1:
